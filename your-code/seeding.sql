184db8698f791adfafac5e3a1e0654cc7b4efe6a
Joana Maia <joanaritamaia99@gmail.com>
2022-10-18 22:56:12 +0200

    done

---
+++ your-code/seeding.sql
@@ -0,0 +1,32 @@
+
+INSERT INTO Cars VALUES
+("K096I98581DHSNUP", "Volkswagen", "Tiguan", 2019, "Blue"),
+("ZM8G7BEUQZ97IH46V", "Peugeot", "Rifter", 2019, "Red"),
+("RKXVNNIHLVVZOUB4M", "Ford", "Fusion", 2018, "White"),
+("HKNDGS7CU31E9Z7JW", "Toyota", "RAV4", 2018, "Silver"),
+("DAM41UDN3CHU2WVF6", "Volvo", "V60", 2019, "Gray"),
+("DAM41UDN3CHU2WVF6", "Volvo", "V60 Cross Country", 2019, "Gray");
+
+​INSERT INTO Customers VALUES
+(10001, "Pablo", "Picasso", +34 636 17 63 82, "something@gmail.com" , "Paseo de la Chopera, 14", "Madrid", "Spain", 28045),
+(20001, "Abraham", "Lincoln", +1 305 907 7086, "something2@gmail.com" , "120 SW 8th St", "Florida", "United States", 33130),
+(30001, "Napoléon", "Bonaparte", +33 1 79 75 40 00, "something3@gmail.com", "40 Rue du Colisée", "Île-de-France", "France", 75008);
+
+​INSERT INTO Salespersons VALUES
+(00001, "Petey", "Cruiser", 636 17 63 82, , "s_else@gmail.com", "Madrid", 28),
+(00002, "Anna", "Sthesia", 305 907 7086, , "s_else2@gmail.com", "Barcelona", 33),
+(00003, "Paul", "Molive", 1 79 75 40 00, , "s_else3@gmail.com", "Berlin", 75),
+(00004, "Gail", "Forcewind", 444 75 40 00, , "s_else4@gmail.com", "Paris", 39),
+(00005, "Paige", "Turner", 872 75 40 00, , "s_else5@gmail.com", "Mimia", 23),
+(00006, "Bob", "Frapples", 822 75 40 00, , "s_else6@gmail.com", "Mexico City", 13),
+(00007, "Walter", "Melon", 083 75 40 00, , "s_else7@gmail.com", "Amsterdam", 40),
+(00008, "Shonda", "Leer", 213 75 40 00, , "s_else8@gmail.com", "São Paulo", 59);
+
+​INSERT INTO Invoices VALUES
+(852399038, 35000, 22-08-2018),
+(731166526, 60000, 31-12-2018),
+(271135104, 45000, 22-01-2019);
+
+
+
+
