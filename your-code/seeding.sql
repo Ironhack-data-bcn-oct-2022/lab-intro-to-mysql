287329ec561e1b10a58f69a95c754e4124a82edf
Bernat Bellmunt <bbellmunt@gmail.com>
2022-10-18 19:04:31 +0200

    sql exercises with bonus

--- your-code/seeding.sql
+++ your-code/seeding.sql
@@ -7,7 +7,7 @@ INSERT INTO cars values
 (2,"RKXVNNIHLVVZOUB4M","Ford","Fusion",2018,"White"),
 (3,"HKNDGS7CU31E9Z7JW","Toyota","RAV4",2018,"Silver"),
 (4,"DAM41UDN3CHU2WVF6","Volvo","V60",2019,"Gray"),
-(5,"DAM41UDN3CHU2WVF5","Volvo","V60 Cross Country",2019,"Gray")
+(5,"DAM41UDN3CHU2WVF6","Volvo","V60 Cross Country",2019,"Gray")
 ;
 
 INSERT INTO customers values
@@ -21,7 +21,7 @@ INSERT INTO salespersons values
 (1,00002,"Anna Sthesia","Barcelona"),
 (2,00003,"Paul Molive","Berlin"),
 (3,00004,"Gail Forcewind","Paris"),
-(4,00005,"Paige Turner","Mimia"),
+(4,00005,"Paige Turner","Miami"),
 (5,00006,"Bob Frapples","Mexico City"),
 (6,00007,"Walter Melon","Amsterdam"),
 (7,00008,"Shonda Leer","São Paulo")
