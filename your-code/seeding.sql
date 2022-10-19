239aed9d7504a7d2b64a105a7da77e0d5b88433a
Selma <slimaa.l3@gmail.com>
2022-10-19 10:26:29 +0200

    Lab MySQL solutions

---
+++ your-code/seeding.sql
@@ -0,0 +1,35 @@
+INSERT INTO cars (cars_id, VIN, Manufacturer, Model,Year,Color)
+VALUES 
+
+("0","ZM8G7BEUQZ97IH46V","Peugeot","Rifter","2019","Red"),
+("1","RKXVNNIHLVVZOUB4M","Ford","Fusion","2018","White"),
+("2","HKNDGS7CU31E9Z7JW","Toyota","RAV4","2018","Silver"),
+("3","DAM41UDN3CHU2WVF6","Volvo","V60","2019","Gray"),
+("4","DAM41UDN3CHU2WVF6","Volvo","V60 Cross Country","2019","Gray");
+
+INSERT INTO Customers 
+VALUES 
+("10001", "Pablo Picasso",	"+34 636 17 63 82",	"-", "Paseo de la Chopera, 14",	"Madrid", "Madrid",	"Spain", "28045"),
+("20001", "Abraham Lincoln", "+1 305 907 7086", "-", "120 SW 8th St", "Miami",	"Florida", "United States",	"33130"),
+("30001", "Napoléon Bonaparte", "+33 1 79 75 40 00", "-", "40 Rue du Colisée", "Paris",	"Île-de-France", "France", "75008");
+
+INSERT INTO Salespersons 
+VALUES
+("00001", "Petey Cruiser", "Madrid"),
+("00002", "Anna Sthesia", "Barcelona"),
+("00003", "Paul Molive", "Berlin"),
+("00004", "Gail Forcewind",	"Paris"),
+("00005", "Paige Turner", "Mimia"),
+("00006", "Bob Frapples" ,"Mexico City"),
+("00007", "Walter Melon", "Amsterdam"),
+("00008", "Shonda Leer", "São Paulo");
+
+INSERT INTO Invoices
+VALUES
+("852399038", "2018-08-22",	"0", "1", "3"),
+("731166526","2018-12-31", "3", "0", "5"),
+("271135104", "2019-01-22", "2", "2","7");
+
+
+
+
